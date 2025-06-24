3f60425e9c0c0a4a2551da5451bc21b33fdc9866
Carol Kosgei <caroljemutai35@gmail.com>
2025-06-24 17:24:34 +0300

    added homepage

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Homepage from './pages/home'
+
+const routes = createBrowserRouter([
+  {
+    Path:"/home", 
+    element:<Homepage/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={routes} />
+  </StrictMode>
 )
+
