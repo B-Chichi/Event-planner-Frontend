e868198e533e7d7bf17a7f535989e7ff0d3bea15
Brian <brian2000bett@gmail.com>
2025-06-24 11:40:06 +0300

    event form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { LoginForm } from "./components/login-form";
 import { Layout } from "./components/Layout/Layout";
+import EventForm from "./components/Layout/Event-Form";
 
 function App() {
   return (
     <>
       <LoginForm />
-      <Layout/>
+      <Layout />
+      <EventForm/>
     </>
   );
 }
