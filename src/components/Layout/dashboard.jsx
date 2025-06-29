75e5aebe17a5948f59393205f98554cb268a252c
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-06-29 17:18:40 +0300

    modified dashboard.jsx

--- src/components/Layout/dashboard.jsx
+++ src/components/Layout/dashboard.jsx
@@ -2,7 +2,6 @@ import { AppSidebar } from "@/components/app-sidebar";
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -24,12 +23,6 @@ export function Dashboard({ children }) {
     <SidebarProvider>
       <AppSidebar className="text-gray-900 dark:bg-gray-900 dark:text-black px-6 py-12" />
       <SidebarInset className="min-h-screen text-gray-900 dark:bg-gray-900 dark:text-black px-6 py-12">
-        <header className="flex h-5 shrink-0 items-center pt-4">
-          <div className="flex flex-1 items-center px-3">
-            <SidebarTrigger />
-          </div>
-        </header>
-
         <div className="flex flex-1 flex-col gap-3 px-4">{children}</div>
       </SidebarInset>
     </SidebarProvider>
